89d361f36702b5a311c40717ba43df3bf4801b1b
IJMTutorSES <s.specht@ijm-online.de>
2021-11-25 13:07:24 +0100

    version 1.5.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name = "ijmfttxt",
-    version= "1.5",
+    version= "1.5.1",
     description= "Erweiterung zu ftrobopy",
     long_description= readme(),
     classifiers=[
