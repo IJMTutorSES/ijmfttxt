1cbc2e3d130ca7e44f79f5ddedd056b8cac90b7b
IJMTutorSES <s.specht@ijm-online.de>
2022-05-09 17:12:14 +0200

    1.9.6

--- ijmfttxt/__init__.py
+++ ijmfttxt/__init__.py
@@ -1,8 +1,9 @@
-from .txt import *
-from .keyboard import *
+from .txt import TXT
+from .keyboard import Keyboard, Mouse
 from .clock import Clock
 
-__version__ = "1.9.5"
+__version__ = "1.9.6"
+print(f"using ijmfttxt {__version__}")
 
 sleep = Clock.sleep
 wait = Clock.wait
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="ijmfttxt",
-    version="1.9.5",
+    version="1.9.6",
     description="Erweiterung zu ftrobopy",
     long_description=readme(),
     classifiers=[
